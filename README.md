c938e6c435b2e318ac46095540182a1d2c42a964
Eshanvi-Joshi <76736640+Eshanvi-Joshi@users.noreply.github.com>
2021-04-08 21:01:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-24-Classes-and-objects
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,65 @@
+//Step 1 - Declaring the variables/constants
+//namespacing OR alias OR nickname
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+//Step 2 - declare variables for your own engine and world
+var engine, world;
+var box;
+var ground;
+var ball;
+
+function setup() {
+  createCanvas(1200,400);
+
+  //Step 3
+  engine = Engine.create();
+  world = engine.world;
+
+  //Step 4 - Create Bodies
+  //rectangle and circle
+  ground = new Ground(600,height,1200,20);
+
+  box1 = new Box(700, 320, 70, 70);
+  box2 = new Box(920, 320, 70, 70);
+  pig1 = new Pig(810, 350);
+  log1 = new Log(810,260,300,PI/2);
+
+  box3 = new Box(700, 240, 70, 70);
+  box4 = new Box(920, 240, 70, 70);
+  pig2 = new Pig(810, 220);
+  log2 = new Log(810,180,300,PI/2);
+
+  box5 = new Box(810,160,70,70);
+  log3 = new Log(760,120,150,PI/7);
+  log4 = new Log(870,120,150,-PI/7);
+
+  bird = new Bird(100,100);
+
+}
+
+function draw() {
+  background("black");  
+  //Step 6 - Run the engine
+  Engine.update(engine);
+  
+  //Step 7 - Display your bodies/box
+  ground.display();
+
+  box1.display();
+  box2.display();
+  pig1.display();
+  log1.display();
+
+  box3.display();
+  box4.display();
+  pig2.display();
+  log2.display();
+
+  box5.display();
+  log3.display();
+  log4.display();
+
+  bird.display();
+}
